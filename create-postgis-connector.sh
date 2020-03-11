9a75141a54b349c194b4b5bc41957907090a9860
Rune Nielsen <runeanielsen@live.dk>
2020-03-11 15:04:25 +0100

    adds postgis connector

---
+++ create-postgis-connector.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '
+{
+ "name": "postgis-connector",
+ "config": {
+ "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+ "tasks.max": "1",
+ "plugin.name": "pgoutput",
+ "database.hostname": "postgres",
+ "database.port": "5432",
+ "database.user": "postgres",
+ "database.password": "postgres",
+ "database.dbname" : "FTTH_TEST",
+ "database.server.name": "dbserver1",
+ "database.whitelist": "FTTH_TEST",
+ "database.history.kafka.bootstrap.servers": "kafka:29092",
+ "database.history.kafka.topic": "schema-changes.FTTH_TEST"
+ }
+}'
